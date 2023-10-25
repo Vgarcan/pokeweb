268312f784bb1fcc03a19e715465e016adff5bcd
Victor Garcia Cantalapiedra <100930736+Vgarcan@users.noreply.github.com>
2023-10-25 15:04:24 +0100

    arreglos

--- timer.py
+++ timer.py
@@ -1,14 +1,11 @@
 import time
 
-current_time= int(time.time())
-print(current_time)
-
 
 def speed_deco(function):
     def wrapper(*args, **kwargs):
-        start_time = int(time.time())
+        start_time = float(time.time()) # 
         result = function(*args, **kwargs)## nombrado valor para devolver solo la funcion
-        end_time = int(time.time())
+        end_time = float(time.time())
         print(f"\n\n{function.__name__} : RUN SPEED: {end_time - start_time} seconds\n\n")
         return result##mirar esto##
     return wrapper
