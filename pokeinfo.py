fa44295598a1843d46ab19211a58558558093ec5
Victor Garcia Cantalapiedra <100930736+Vgarcan@users.noreply.github.com>
2023-10-28 20:58:13 +0100

    idioma de Pokeinfo cambiado al ingles

--- pokeinfo.py
+++ pokeinfo.py
@@ -4,84 +4,81 @@ poke_point = 'https://pokeapi.co/api/v2/pokemon/122/'
 POKEAPI = requests.get(poke_point).json()
 
 
-# Por cada KEY en el diccionario 
 def loop_items(dict_item, lvl=0, mode=type):
     '''
-    Esta función recorre un diccionario anidado e imprime información sobre sus claves y valores.
+    This function iterates through a nested dictionary and prints information about its keys and values.
 
     Args:
-    dict_item (dict): El diccionario que deseas analizar.
-    lvl (int): Nivel de anidamiento (por defecto, 0).
-    mode (type): Función que determina cómo mostrar el tipo de valor (por defecto, type).
+    dict_item (dict): The dictionary you want to analyze.
+    lvl (int): Nesting level (default is 0).
+    mode (type): A function that determines how to display the value's type (default is type).
 
-    Uso:
-    1. Proporciona el diccionario que deseas analizar como 'dict_item'.
-    2. Opcionalmente, puedes especificar el nivel de anidamiento inicial 'lvl' (por defecto, 0).
-    3. Opcionalmente, puedes cambiar la función para mostrar el tipo de valor utilizando 'mode' (por defecto, type).
+    Usage:
+    1. Provide the dictionary you want to analyze as 'dict_item'.
+    2. Optionally, you can specify the initial nesting level 'lvl' (default is 0).
+    3. Optionally, you can change the function for displaying the value's type using 'mode' (default is type).
 
-    La función recorre el diccionario, mostrando las claves en mayúsculas, el tipo de contenido en cada clave
-    y, si un valor es una lista o un diccionario, muestra información adicional.
+    The function iterates through the dictionary, displaying keys in uppercase, the content type for each key,
+    and, if a value is a list or dictionary, it shows additional information.
 
-    Ejemplo de uso:
-    dict_ejemplo = {
-        'nombre': 'Juan',
-        'edades': [25, 30, 35],
-        'datos': {
-            'altura': 180,
-            'peso': 75
+    Usage Example:
+    example_dict = {
+        'name': 'John',
+        'ages': [25, 30, 35],
+        'data': {
+            'height': 180,
+            'weight': 75
         }
     }
-    loop_items(dict_ejemplo)
+    loop_items(example_dict)
 
-    Este ejemplo imprimirá la información sobre las claves y valores en 'dict_ejemplo' junto con el tipo de contenido.
+    This example will print information about the keys and values in 'example_dict' along with their content types.
 
-    Nota:
-    Si el valor es una lista o un diccionario anidado, la función explorará también esos elementos anidados.
+    Note:
+    If a value is a nested list or dictionary, the function will also explore those nested elements.
 
     '''
 
     INDENTATION = '--//' * lvl
 
     for key_item in dict_item.items():
-        # imprime KEY's NAME + Tipo de contenido en ese KEY
+        # Print KEY's NAME + Type of content in that KEY
         if type(key_item[1]) == dict or type(key_item[1]) == list:
             print(INDENTATION, key_item[0].upper(),'>>',type(key_item[1]),'<<')
         
         else:
             print(INDENTATION, key_item[0].upper(),'>>',mode(key_item[1]),'<<')
         
-        # Ahora analzimamos KEY_ITEM[1]
+        # Now we analyze KEY_ITEM[1]
         ###############################
 
-        # Si KEY_ITEM[1] es una LIST 
+        # If KEY_ITEM[1] is a LIST 
         if isinstance(key_item[1], list):
-            # Nos muestra el numero de elementos en esa LIST
+            # Display the number of items in that LIST
             print (INDENTATION, f'-- Num. of Items = {len(key_item[1])}')
 
-            #  Por cada ITEM en KEY_ITEM[1](List)
+            # For each ITEM in KEY_ITEM[1](List)
             for listitem in key_item[1]:
                 
-                # Si el elemnto de la lista es un DICT_item
+                # If the list item is a DICT_item
                 if isinstance(listitem, dict):
                     
-                    # Por cada KEY en el diccionario 
+                    # For each KEY in the dictionary 
                     loop_items(listitem, lvl= lvl+1, mode=mode)
                                      
-                # Si el elemento de la lista es un LIST
+                # If the list item is a LIST
                 elif isinstance(listitem, list):
                     
-                    # Por cada KEY en el diccionario 
+                    # For each KEY in the dictionary 
                     loop_items(listitem, lvl= lvl+1, mode=mode)
           
         ##################################################################
             
-        # Si KEY_ITEM[1] es un DICT_item                
+        # If KEY_ITEM[1] is a DICT_item                
         elif isinstance(key_item[1], dict):
 
-            # Por cada KEY en el diccionario 
+            # For each KEY in the dictionary 
             loop_items(key_item[1], lvl= lvl+1, mode=mode)
-        
-
 
 
 
